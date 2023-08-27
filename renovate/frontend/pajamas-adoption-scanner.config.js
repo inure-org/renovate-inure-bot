fa87f8452a1ba87b6d77f4605014c1d164fd2bba
grivo <90540827+grvo@users.noreply.github.com>
2023-08-27 02:08:07 -0300

    scanner de adoção para pajamas no bot

---
+++ renovate/frontend/pajamas-adoption-scanner.config.js
@@ -0,0 +1,44 @@
+// consts de dependências locais
+const {
+    createServerConfig,
+    baseConfig,
+    foundationLabels,
+    updateNothing
+} = require("../lib/shared");
+
+const {
+    prVueMajor2,
+    prLabel,
+    prJest,
+    prGitLabUISVG
+} = require("../lib/npm");
+
+module.exports = createServerConfig([
+    {
+        repository: "inure-renovate-forks/pajamas-adoption-scanner",
+        ...baseConfig,
+        labels: foundationLabels,
+
+        includePaths: [
+            // package de alto-nível
+            "*",
+
+            // a workspace do dashboard
+            "dashboard/**"
+        ],
+
+        reviewers: ["markrian"],
+        rangeStrategy: "auto",
+        enabledManagers: ["npm"],
+
+        packageRules: [
+            updateNothing,
+            prGitLabUISVG,
+            prVueMajor2,
+            prBabel,
+            prJest
+        ],
+
+        updateInternalDeps: true
+    }
+]);

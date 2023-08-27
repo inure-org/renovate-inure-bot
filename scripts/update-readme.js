c57f49c4648e2c28cda08d9ffdc3c08bfd762552
grivo <90540827+grvo@users.noreply.github.com>
2023-08-27 02:42:09 -0300

    implementação de script para atualização de readme

---
+++ scripts/update-readme.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+// consts de dependências
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+
+// const de dependência local
+const configFiles = glob.sync(
+    path.join(__dirname, "..", "renovate", "**", "*.config.js")
+);
+
+const repositories = configFiles.map(require)
+    .flatMap((x) => x.repositories);
+
+console.log(repositories);
+
+const listItem = (path) => `- [${path}](https://gitlab.com/${path})`;
+
+const list = [
+    ...new Set(
+        repositories.map((x) => {
+            return listItem(x?.repository);
+        })
+    )
+]
+    .sort()
+    .join("\n");
+
+const delimiter = "<!-- rep -->";
+const regex = new RegExp(`${delimiter}[\\s\\S]+${delimiter}`, "gm");
+
+const section = `
+${delimiter}
+
+${list}
+
+${delimiter}
+`;
+
+const readme = fs.readFilySync("README.md", "utf8");
+
+fs.writeFileSync("README.md", readme.replace(regex, section));
+
+console.warn(list);
